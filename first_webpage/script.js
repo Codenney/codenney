4a17b977f88e1b1a3aee9154ef856489e89ab62c
Kehinde Adedokun <kenney9224@gmail.com>
2020-03-23 08:49:01 -0700

    My JavaScript File

---
+++ first_webpage/script.js
@@ -0,0 +1,13 @@
+var my_name = "Kehinde Adedokun";
+
+const my_courses = ['HTML', 'CSS', 'JavaScript'];
+
+console.log(my_name);
+console.log(my_courses);
+
+for (let i = 0; i <= 200; i++){
+    if((i % 2) !== 0){
+        console.log(i);
+    }
+}
+
